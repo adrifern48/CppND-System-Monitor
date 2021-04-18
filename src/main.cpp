82d04e6f12fac9efb81f92cce32d5ccbd0f5cd70
adrifern48 <adrifern48@gmail.com>
2021-04-18 21:10:41 +0000

    clean up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include "ncurses_display.h"
 #include "system.h"
 
-#include <iostream> //used for testing
-#include "linux_parser.h"
-
 int main() {
   System system;
-  // NCursesDisplay::Display(system);
-  auto p = Process(1);
-  std::cout << LinuxParser::Command(1) << "\n";
-  
+  NCursesDisplay::Display(system);
 }
